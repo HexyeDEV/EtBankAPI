c15afdbbec926ada32a2f03ffbbeed8e645973ae
Hexye <dragonsale22@gmail.com>
2026-01-25 14:32:38 +0100

    Bump version to 0.3 and update EtBankAPI to use double for amount handling

--- src/main/java/me/hexye/etbank/api/EtBankAPI.java
+++ src/main/java/me/hexye/etbank/api/EtBankAPI.java
@@ -3,7 +3,8 @@ package me.hexye.etbank.api;
 import java.util.UUID;
 
 public interface EtBankAPI {
-    void deposit(UUID playerId, int amount);
-    void withdraw(UUID playerId, int amount);
-    int getBalance(UUID playerId);
+    void deposit(UUID playerId, double amount);
+    void withdraw(UUID playerId, double amount);
+    void setBalance(UUID playerId, double amount);
+    double getBalance(UUID playerId);
 }
